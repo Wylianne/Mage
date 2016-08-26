720989a0c41b7e841bcb467573986de1c333418d
Wylianne <wyliannec@gmail.com>
2016-08-26 11:48:53 -0200

    Create exibir-ocultar-itens-home.php

---
+++ Outros/exibir-ocultar-itens-home.php
@@ -0,0 +1,7 @@
+	<?php 
+	    $url = "http://".$_SERVER['SERVER_NAME'].$_SERVER ['REQUEST_URI']; ?>
+
+		  if ((Mage::getBaseUrl() == $url."index.php/") or (Mage::getBaseUrl() == $url)){
+			        //codigo
+		  }
+	?>
