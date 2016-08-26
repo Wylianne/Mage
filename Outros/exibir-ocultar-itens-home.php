d3e1d6ec99e005e4a7bf13ba9e6390217402ffe8
Wylianne <wyliannec@gmail.com>
2016-08-26 11:50:28 -0200

    Update exibir-ocultar-itens-home.php

--- Outros/exibir-ocultar-itens-home.php
+++ Outros/exibir-ocultar-itens-home.php
@@ -1,7 +1,9 @@
 	<?php 
-	    $url = "http://".$_SERVER['SERVER_NAME'].$_SERVER ['REQUEST_URI']; ?>
+		//Código utilizado no list.phtml
+		///public_html/app/design/frontend/--template--/default/template/catalog/product
+		$url = "http://".$_SERVER['SERVER_NAME'].$_SERVER ['REQUEST_URI']; ?>
 
-		  if ((Mage::getBaseUrl() == $url."index.php/") or (Mage::getBaseUrl() == $url)){
-			        //codigo
-		  }
+		if ((Mage::getBaseUrl() == $url."index.php/") or (Mage::getBaseUrl() == $url)){
+			//codigo
+		}
 	?>
